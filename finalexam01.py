918fc26113e8e7038fd09e94eddd111a85bc4e88
Youkwangchae <gc9612@gmail.com>
2020-06-24 10:16:06 +0900

    this is real first

--- finalexam01.py
+++ finalexam01.py
@@ -8,7 +8,7 @@ while cnt<902:
     c = 1/a*(-1)**b
     total = total + c
     if(cnt%100 ==1):
-        print(cnt,"\t",round(4*total,4))
+        print(cnt,"\t",format(4*total,".4f"))
     a=a+2
     cnt = cnt+1
     b=b+1
